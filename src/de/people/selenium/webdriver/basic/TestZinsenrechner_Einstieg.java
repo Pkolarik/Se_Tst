4bdeec0a9adcdb469c7a1a09f28434bbd9d022ac
kpawlyta <katharina@pawlyta.de>
2019-07-26 13:42:41 +0200

    neue Klasse von Katharina

---
+++ src/de/people/selenium/webdriver/basic/TestZinsenrechner_Einstieg.java
@@ -0,0 +1,10 @@
+package de.people.selenium.webdriver.basic;
+
+public class TestZinsenrechner_Einstieg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
