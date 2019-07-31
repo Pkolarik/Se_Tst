adaa635e54d16b2bf26aa16041e90a69273b4c5f
A349166 <A349166@BE1DA649.emea1.cds.t-internal.com>
2019-07-31 11:36:47 +0200

    Mein zweiter Commit.

--- src/de/mtreiss/Dummy.java
+++ src/de/mtreiss/Dummy.java
@@ -4,7 +4,8 @@ public class Dummy {
 
 	public static void main(String[] args) {
 		System.out.println("Dummy");
-		
+		// Schöne Grüsse an alle, auch von Pavel
+		// Nächsten Kaffee zahle ich per REVOLUT :-)
 	}
 
 }
